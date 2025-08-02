6d26d89e30286fad46bcd3fd6df7ca9914e16f87
metrovian <paramedoct@gmail.com>
2025-08-02 22:35:09 +0900

    CHANGE. [DECYRPT] - 암호해독 인터페이스 로그 표기방식 변경 - 암호해독 인터페이스 완전가상함수 파라미터 오버로딩

--- include/decryption_abstract.h
+++ include/decryption_abstract.h
@@ -9,13 +9,18 @@
 #include <spdlog/spdlog.h>
 
 class decryption_abstract {
-protected: /* stream transform */
+public: /* stream transform */
 	static std::vector<uint8_t> stob(const std::string &chars);
 	static std::vector<uint8_t> htob(const std::string &hexs);
 	static std::string btos(const std::vector<uint8_t> &bytes);
 	static std::string btoh(const std::vector<uint8_t> &bytes);
 
-public: /* abstract interface */
+public: /* overload */
+	int8_t decrypt(const std::vector<uint8_t> &cipher, std::string &plain);
+	int8_t decrypt(const std::string &cipher, std::string &plain);
+	int8_t decrypt(const std::string &cipher, std::vector<uint8_t> &plain);
+
+public: /* abstract */
 	virtual int8_t decrypt(const std::vector<uint8_t> &cipher, std::vector<uint8_t> &plain) = 0;
 	virtual ~decryption_abstract() {}
 };
