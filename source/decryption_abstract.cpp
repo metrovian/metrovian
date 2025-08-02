8642a6a5553c1ce8c9632f2564e7573188b77808
metrovian <paramedoct@gmail.com>
2025-08-02 22:35:09 +0900

    CHANGE. [DECYRPT] - 암호해독 인터페이스 로그 표기방식 변경 - 암호해독 인터페이스 완전가상함수 파라미터 오버로딩

--- source/decryption_abstract.cpp
+++ source/decryption_abstract.cpp
@@ -5,10 +5,10 @@ std::vector<uint8_t> decryption_abstract::stob(const std::string &chars) {
 }
 
 std::vector<uint8_t> decryption_abstract::htob(const std::string &hexs) {
-	spdlog::trace("[decryption_abstract::htob] enter");
+	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
 	if (hexs.size() % 2 != 0) {
-		spdlog::critical("[decryption_abstract::htob] invalid arguments: hexs.size() % 2 != 0");
-		spdlog::trace("[decryption_abstract::htob] exit");
+		spdlog::critical("[invalid arguments] {}", __PRETTY_FUNCTION__);
+		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
 		return std::vector<uint8_t>();
 	}
 
@@ -17,7 +17,7 @@ std::vector<uint8_t> decryption_abstract::htob(const std::string &hexs) {
 		bytes.push_back(std::stoi(hexs.substr(i, 2), nullptr, 16));
 	}
 
-	spdlog::trace("[decryption_abstract::htob] exit");
+	spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
 	return bytes;
 }
 
@@ -26,13 +26,40 @@ std::string decryption_abstract::btos(const std::vector<uint8_t> &bytes) {
 }
 
 std::string decryption_abstract::btoh(const std::vector<uint8_t> &bytes) {
-	spdlog::trace("[decryption_abstract::btoh] enter");
+	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
 	std::ostringstream oss;
 	oss << std::hex << std::setfill('0');
 	for (uint8_t byte : bytes) {
 		oss << std::setw(2) << static_cast<int32_t>(byte);
 	}
 
-	spdlog::trace("[decryption_abstract::btoh] exit");
+	spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
 	return oss.str();
+}
+
+int8_t decryption_abstract::decrypt(const std::vector<uint8_t> &cipher, std::string &plain) {
+	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
+	std::vector<uint8_t> bytes;
+	int8_t retcode = decrypt(cipher, bytes);
+	plain = btos(bytes);
+	spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
+	return retcode;
+}
+
+int8_t decryption_abstract::decrypt(const std::string &cipher, std::string &plain) {
+	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
+	std::vector<uint8_t> chars = stob(cipher);
+	std::vector<uint8_t> bytes;
+	int8_t retcode = decrypt(cipher, bytes);
+	plain = btos(bytes);
+	spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
+	return retcode;
+}
+
+int8_t decryption_abstract::decrypt(const std::string &cipher, std::vector<uint8_t> &plain) {
+	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
+	std::vector<uint8_t> chars = stob(cipher);
+	int8_t retcode = decrypt(cipher, plain);
+	spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
+	return retcode;
 }
