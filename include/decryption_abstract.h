5bd217951d5b87ef78ac5a56ac6b7b5fed04f616
metrovian <paramedoct@gmail.com>
2025-08-03 16:20:07 +0900

    ADD. [DECRYPT] - OpenSSL Base64 비트스트림 디코딩 체인 구현

--- include/decryption_abstract.h
+++ include/decryption_abstract.h
@@ -7,9 +7,12 @@
 #include <string>
 #include <stdint.h>
 #include <spdlog/spdlog.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/buffer.h>
 
 class decryption_abstract {
-public: /* stream transform */
+protected: /* stream transform */
 	static std::vector<uint8_t> stob(const std::string &chars);
 	static std::vector<uint8_t> htob(const std::string &hexs);
 	static std::string btos(const std::vector<uint8_t> &bytes);
