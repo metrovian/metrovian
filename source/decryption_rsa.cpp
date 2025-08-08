74dde212edba734f77c018c69d1596d7f7f00251
metrovian <paramedoct@gmail.com>
2025-08-08 23:42:59 +0900

    FIXED. [DECRYPT] - RSA 개인키 소인수분해 공격 메모리 해제 개선

--- source/decryption_rsa.cpp
+++ source/decryption_rsa.cpp
@@ -1,4 +1,5 @@
 #include "decryption_rsa.h"
+#include "predefined.h"
 
 int8_t decryption_rsa::setkey(const std::vector<uint8_t> &private_key) {
 	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
@@ -62,63 +63,90 @@ int8_t decryption_rsa::calckey(const std::vector<uint8_t> &public_key, rsa::atta
 
 int8_t decryption_rsa::calckey(const std::string &public_key, rsa::attack algorithm) {
 	spdlog::trace("[enter] {}", __PRETTY_FUNCTION__);
-	BIO *bio = BIO_new_mem_buf(public_key.data(), static_cast<int32_t>(public_key.size()));
+	int8_t retcode = 0;
+	BIO *bio = nullptr;
+	EVP_PKEY *pkey_public = nullptr;
+	EVP_PKEY_CTX *ctx_public = nullptr;
+	BIGNUM *n_rsa = nullptr;
+	BIGNUM *e_rsa = nullptr;
+	BN_CTX *ctx_rsa = nullptr;
+	BIGNUM *p_rsa = nullptr;
+	BIGNUM *q_rsa = nullptr;
+	BIGNUM *phi_rsa = nullptr;
+	BIGNUM *p1_rsa = nullptr;
+	BIGNUM *q1_rsa = nullptr;
+	BIGNUM *d_rsa = nullptr;
+	OSSL_PARAM_BLD *param_bld = nullptr;
+	OSSL_PARAM *param = nullptr;
+	EVP_PKEY *pkey_private = nullptr;
+	EVP_PKEY_CTX *ctx_private = nullptr;
+	BIGNUM *coefficient = nullptr;
+	BIGNUM *exponent1 = nullptr;
+	BIGNUM *exponent2 = nullptr;
+	BIO *mem = nullptr;
+	BUF_MEM *buf_mem = nullptr;
+	bio = BIO_new_mem_buf(public_key.data(), static_cast<int32_t>(public_key.size()));
 	if (bio == nullptr) {
 		spdlog::critical("[invalid implementation] {}", __PRETTY_FUNCTION__);
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	EVP_PKEY *pkey_public = PEM_read_bio_PUBKEY(bio, nullptr, nullptr, nullptr);
-	BIO_free(bio);
+	pkey_public = PEM_read_bio_PUBKEY(bio, nullptr, nullptr, nullptr);
 	if (pkey_public == nullptr) {
 		spdlog::error("rsa decryption calckey pem readout failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	EVP_PKEY_CTX *ctx_pub = EVP_PKEY_CTX_new(pkey_public, nullptr);
-	if (ctx_pub == nullptr) {
+	ctx_public = EVP_PKEY_CTX_new(pkey_public, nullptr);
+	if (ctx_public == nullptr) {
 		EVP_PKEY_free(pkey_public);
 		spdlog::critical("[invalid implementation] {}", __PRETTY_FUNCTION__);
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	} else if (EVP_PKEY_get_base_id(pkey_public) != EVP_PKEY_RSA) {
+		EVP_PKEY_CTX_free(ctx_public);
 		EVP_PKEY_free(pkey_public);
 		spdlog::error("rsa decryption calckey baseid readout failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	BIGNUM *n_rsa = nullptr;
-	BIGNUM *e_rsa = nullptr;
 	EVP_PKEY_get_bn_param(pkey_public, OSSL_PKEY_PARAM_RSA_N, &n_rsa);
 	EVP_PKEY_get_bn_param(pkey_public, OSSL_PKEY_PARAM_RSA_E, &e_rsa);
 	if (n_rsa == nullptr) {
+		EVP_PKEY_CTX_free(ctx_public);
 		EVP_PKEY_free(pkey_public);
 		spdlog::error("rsa decryption calckey modulus readout failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	} else if (e_rsa == nullptr) {
+		EVP_PKEY_CTX_free(ctx_public);
 		EVP_PKEY_free(pkey_public);
+		BN_free(n_rsa);
 		spdlog::error("rsa decryption calckey exponent readout failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	BN_CTX *ctx_rsa = BN_CTX_new();
-	BIGNUM *p_rsa = BN_new();
-	BIGNUM *q_rsa = BN_new();
+	ctx_rsa = BN_CTX_new();
+	p_rsa = BN_new();
+	q_rsa = BN_new();
 	switch (algorithm) {
 	case rsa::attack::trial: {
 		BIGNUM *trial = BN_new();
 		BIGNUM *remainder = BN_new();
+		BN_set_word(trial, 2);
 		while (BN_cmp(trial, n_rsa) < 0) {
 			BN_mod(remainder, n_rsa, trial, ctx_rsa);
 			if (BN_is_zero(remainder)) {
 				BN_copy(p_rsa, trial);
 				BN_div(q_rsa, nullptr, n_rsa, p_rsa, ctx_rsa);
 				break;
+			} else {
+				BN_add_word(trial, 1);
+				continue;
 			}
 		}
 
@@ -130,37 +158,27 @@ int8_t decryption_rsa::calckey(const std::string &public_key, rsa::attack algori
 	default:
 		spdlog::critical("[invalid implementation] {}", __PRETTY_FUNCTION__);
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	BIGNUM *phi_rsa = BN_new();
-	BIGNUM *p1_rsa = BN_new();
-	BIGNUM *q1_rsa = BN_new();
-	BIGNUM *d_rsa = BN_new();
+	phi_rsa = BN_new();
+	p1_rsa = BN_new();
+	q1_rsa = BN_new();
+	d_rsa = BN_new();
 	BN_sub(p1_rsa, p_rsa, BN_value_one());
 	BN_sub(q1_rsa, q_rsa, BN_value_one());
 	BN_mul(phi_rsa, p1_rsa, q1_rsa, ctx_rsa);
 	if (BN_mod_inverse(d_rsa, e_rsa, phi_rsa, ctx_rsa) == nullptr) {
-		BN_free(phi_rsa);
-		BN_free(p1_rsa);
-		BN_free(q1_rsa);
-		BN_free(p_rsa);
-		BN_free(q_rsa);
-		BN_free(d_rsa);
-		BN_CTX_free(ctx_rsa);
-		EVP_PKEY_free(pkey_public);
 		spdlog::error("rsa decryption calckey modular inverse failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	OSSL_PARAM_BLD *param_bld = OSSL_PARAM_BLD_new();
-	OSSL_PARAM *param = nullptr;
-	EVP_PKEY *pkey_private = nullptr;
-	EVP_PKEY_CTX *ctx_private = EVP_PKEY_CTX_new_from_name(nullptr, "RSA", nullptr);
-	BIGNUM *coefficient = BN_new();
-	BIGNUM *exponent1 = BN_new();
-	BIGNUM *exponent2 = BN_new();
+	param_bld = OSSL_PARAM_BLD_new();
+	ctx_private = EVP_PKEY_CTX_new_from_name(nullptr, "RSA", nullptr);
+	coefficient = BN_new();
+	exponent1 = BN_new();
+	exponent2 = BN_new();
 	BN_mod_inverse(coefficient, q_rsa, p_rsa, ctx_rsa);
 	BN_mod(exponent1, d_rsa, p1_rsa, ctx_rsa);
 	BN_mod(exponent2, d_rsa, q1_rsa, ctx_rsa);
@@ -172,70 +190,60 @@ int8_t decryption_rsa::calckey(const std::string &public_key, rsa::attack algori
 	OSSL_PARAM_BLD_push_BN(param_bld, "rsa-factor2", q_rsa);
 	OSSL_PARAM_BLD_push_BN(param_bld, "rsa-exponent1", exponent1);
 	OSSL_PARAM_BLD_push_BN(param_bld, "rsa-exponent2", exponent2);
+	mem = BIO_new(BIO_s_mem());
+	if (mem == nullptr) {
+		spdlog::critical("[invalid implementation] {}", __PRETTY_FUNCTION__);
+		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
+		RETURN_CLEANUP(retcode, -1);
+	}
+
 	param = OSSL_PARAM_BLD_to_param(param_bld);
 	EVP_PKEY_fromdata_init(ctx_private);
 	EVP_PKEY_fromdata(ctx_private, &pkey_private, EVP_PKEY_KEYPAIR, param);
-	BIO *mem = BIO_new(BIO_s_mem());
-	if (mem == nullptr) {
-		EVP_PKEY_free(pkey_public);
-		EVP_PKEY_free(pkey_private);
-		BN_CTX_free(ctx_rsa);
-		spdlog::critical("[invalid implementation] {}", __PRETTY_FUNCTION__);
-		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
-	}
-
 	if (i2d_PrivateKey_bio(mem, pkey_private) != 1) {
-		BIO_free(mem);
-		EVP_PKEY_free(pkey_public);
-		EVP_PKEY_free(pkey_private);
-		BN_CTX_free(ctx_rsa);
 		spdlog::error("rsa decryption calckey der conversion failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
-	BUF_MEM *buf_mem = nullptr;
 	BIO_get_mem_ptr(mem, &buf_mem);
 	if (buf_mem == nullptr) {
-		BIO_free(mem);
-		EVP_PKEY_free(pkey_public);
-		EVP_PKEY_free(pkey_private);
-		BN_CTX_free(ctx_rsa);
 		spdlog::critical("[invalid implementation] {}", __PRETTY_FUNCTION__);
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	} else if (buf_mem->data == nullptr) {
-		BIO_free(mem);
-		EVP_PKEY_free(pkey_public);
-		EVP_PKEY_free(pkey_private);
-		BN_CTX_free(ctx_rsa);
 		spdlog::error("rsa decryption calckey der allocation failed");
 		spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-		return -1;
+		RETURN_CLEANUP(retcode, -1);
 	}
 
 	private_key_.clear();
 	private_key_.assign(buf_mem->data, buf_mem->data + buf_mem->length);
-	BIO_free(mem);
-	EVP_PKEY_free(pkey_public);
-	EVP_PKEY_free(pkey_private);
-	EVP_PKEY_CTX_free(ctx_private);
-	BN_free(exponent1);
-	BN_free(exponent2);
-	BN_free(coefficient);
-	BN_free(phi_rsa);
-	BN_free(p1_rsa);
-	BN_free(p_rsa);
-	BN_free(q_rsa);
-	BN_free(d_rsa);
-	BN_free(n_rsa);
-	BN_free(e_rsa);
-	BN_CTX_free(ctx_rsa);
-	OSSL_PARAM_free(param);
-	OSSL_PARAM_BLD_free(param_bld);
-	spdlog::trace("[exit] {}", __PRETTY_FUNCTION__);
-	return 0;
+
+cleanup:
+	// clang-format off
+	if (bio) BIO_free(bio);
+	if (pkey_public) EVP_PKEY_free(pkey_public);
+	if (ctx_public) EVP_PKEY_CTX_free(ctx_public);
+	if (ctx_rsa) BN_CTX_free(ctx_rsa);
+	if (phi_rsa) BN_clear_free(phi_rsa);
+	if (p1_rsa) BN_clear_free(p1_rsa);
+	if (q1_rsa) BN_clear_free(q1_rsa);
+	if (p_rsa) BN_clear_free(p_rsa);
+	if (q_rsa) BN_clear_free(q_rsa);
+	if (d_rsa) BN_clear_free(d_rsa);
+	if (n_rsa) BN_free(n_rsa);
+	if (e_rsa) BN_free(e_rsa);
+	if (param_bld) OSSL_PARAM_BLD_free(param_bld);
+	if (param) OSSL_PARAM_free(param);
+	if (pkey_private) EVP_PKEY_free(pkey_private);
+	if (ctx_private) EVP_PKEY_CTX_free(ctx_private);
+	if (coefficient) BN_free(coefficient);
+	if (exponent1) BN_free(exponent1);
+	if (exponent2) BN_free(exponent2);
+	if (mem) BIO_free(mem);
+	// clang-format on
+	return retcode;
 }
 
 int8_t decryption_rsa::decryption(const std::vector<uint8_t> &cipher, std::vector<uint8_t> &plain) {
