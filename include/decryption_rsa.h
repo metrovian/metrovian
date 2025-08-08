40d078ae7df8ec4ee08f651f7c5d13e04069e575
metrovian <paramedoct@gmail.com>
2025-08-08 22:53:42 +0900

    ADD. [DECRYPT] - RSA 개인키 단순 소인수분해 공격 구현

--- include/decryption_rsa.h
+++ include/decryption_rsa.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "decryption_abstract.h"
 
+namespace rsa {
+enum class attack {
+	trial = 0,
+	fermat = 1,
+};
+};
+
 class decryption_rsa : public decryption_abstract {
 protected: /* parameter */
 	std::vector<uint8_t> private_key_;
@@ -9,6 +16,10 @@ protected: /* parameter */
 public: /* setter */
 	int8_t setkey(const std::vector<uint8_t> &private_key);
 	int8_t setkey(const std::string &private_key);
+
+public: /* attack */
+	int8_t calckey(const std::vector<uint8_t> &public_key, rsa::attack algorithm);
+	int8_t calckey(const std::string &public_key, rsa::attack algorithm);
 
 protected: /* abstract */
 	virtual int8_t decryption(const std::vector<uint8_t> &cipher, std::vector<uint8_t> &plain) override final;
