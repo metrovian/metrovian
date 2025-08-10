010b32bebf99f3236d97bb26d37b3014bad40ad8
metrovian <paramedoct@gmail.com>
2025-08-10 16:44:02 +0900

    ADD. [DECRYPT] - RSA 개인키 폴라드 약한 소인수분해 공격 구현

--- include/predefined.h
+++ include/predefined.h
@@ -2,4 +2,5 @@
 #define RETURN_CLEANUP(retcode, code)   do { retcode = code; goto cleanup; } while(0)
 #define RSA_FERMAT_MAX_ITERATION        1000000
 #define RSA_POLLARDS_RHO_MAX_ITERATION  100000000
+#define RSA_POLLARDS_P1_MAX_ITERATION   100000
 // clang-format on
