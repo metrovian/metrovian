a44e829989da1df247a351b36dac0f3ecc5bc1f2
metrovian <paramedoct@gmail.com>
2025-08-17 00:23:05 +0900

    FORMAT. [COMMON] - 인수에 의한 명백한 구현실패 로그 규격화

--- include/predefined.h
+++ include/predefined.h
@@ -3,6 +3,7 @@
 #define LOG_ENTER()                     spdlog::trace("[enter] [{}:{}] {}", basename(__FILE__), __LINE__, __func__)
 #define LOG_EXIT()                      spdlog::trace("[exit] [{}:{}] {}", basename(__FILE__), __LINE__, __func__)
 #define LOG_CONDITION(condition)        spdlog::error("[condition] [{}:{}] {}", basename(__FILE__), __LINE__, #condition);
+#define LOG_ARGUMENT(argument)          spdlog::critical("[argument] [{}:{}] {}", basename(__FILE__), __LINE__, #argument);
 #define RSA_FERMAT_MAX_ITERATION        1000000
 #define RSA_POLLARDS_RHO_MAX_ITERATION  100000000
 #define RSA_POLLARDS_P1_MAX_ITERATION   100000
