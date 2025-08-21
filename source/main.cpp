908d35ba6c6c2ace6459cc05ff596bee4f7600da
metrovian <paramedoct@gmail.com>
2025-08-21 22:10:51 +0900

    ADD. [MAIN] - 명령줄 인터페이스 AES-256-CTR 암호해독 구현

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,7 @@
 
 int32_t main(int argc, char **argv) {
 	interface_singleton::instance().command_setup(std::make_unique<command_aes256_cbc>());
+	interface_singleton::instance().command_setup(std::make_unique<command_aes256_ctr>());
 	interface_singleton::instance().command_parse(argc, argv);
 	return 0;
 }
