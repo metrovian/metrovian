45ef07db52fb44f9ec5741766eecd3b7b0e52595
metrovian <paramedoct@gmail.com>
2025-08-22 22:28:01 +0900

    ADD. [MAIN] - 명령줄 인터페이스 RSA 암호해독 구현

--- source/command_abstract.cpp
+++ source/command_abstract.cpp
@@ -123,6 +123,34 @@ void command_aes256_ctr::run() {
 	return;
 }
 
+void command_rsa::setup(CLI::App *parent) {
+	auto command = parent->add_subcommand("rsa", "RSA decryption");
+	command->add_option("-p, --pem", private_pem_, "private pem")->required();
+	command->add_option("-i, --in", in_, "ciphertext binary")->required();
+	command->add_option("-o, --out", out_, "plaintext binary")->required();
+	command->callback([this]() { run(); });
+	return;
+}
+
+void command_rsa::run() {
+	decryption_rsa engine;
+	std::string private_key;
+	if (read_text(private_pem_, private_key) == 0) {
+		if (engine.setkey(private_key) == 0) {
+			std::vector<uint8_t> cipher;
+			std::vector<uint8_t> plain;
+			if (read_binary(in_, cipher) == 0) {
+				if (engine.decrypt(cipher, plain) == 0) {
+					if (write_binary(out_, plain) == 0) {
+						spdlog::info("RSA decryption success");
+						return;
+					}
+				}
+			}
+		}
+	}
+}
+
 void command_avcodec::setup(CLI::App *parent) {
 	auto command = parent->add_subcommand("avcodec", "FFMPEG decompression");
 	command->add_option("-i, --in", path_, "encoded audio")->required();
--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 int32_t main(int argc, char **argv) {
 	interface_singleton::instance().command_setup(std::make_unique<command_aes256_cbc>());
 	interface_singleton::instance().command_setup(std::make_unique<command_aes256_ctr>());
+	interface_singleton::instance().command_setup(std::make_unique<command_rsa>());
 	interface_singleton::instance().command_setup(std::make_unique<command_avcodec>());
 	interface_singleton::instance().command_parse(argc, argv);
 	return 0;
