96bec62ff98f4cf1eabea97f8a180adccf0233bf
metrovian <paramedoct@gmail.com>
2025-08-23 23:20:53 +0900

    ADD. [MAIN] - 명령줄 인터페이스 RSA 공격 알고리즘 구현

--- source/command_abstract.cpp
+++ source/command_abstract.cpp
@@ -1,8 +1,5 @@
 #include "command_abstract.h"
-#include "decryption_aes.h"
-#include "decryption_rsa.h"
-#include "decompression_avcodec.h"
-#include "optimization_user.h"
+#include "predefined.h"
 
 int8_t command_abstract::read_binary(const std::string &path, std::vector<uint8_t> &binary) {
 	std::ifstream ifs(path, std::ios::binary);
@@ -192,6 +189,40 @@ void command_rsa::run() {
 					if (write_binary(out_, plain) == 0) {
 						std::cout << engine.base64(plain) << std::endl;
 						return;
+					}
+				}
+			}
+		}
+	}
+}
+
+void command_rsa_attack::setup(CLI::App *parent) {
+	auto command = parent->add_subcommand("rsa-attack", "RSA-EXPLOIT decryption");
+	command->add_option("-p, --pem", public_pem_, "public pem")->required();
+	command->add_option("-m, --method", method_, "attack method")->required();
+	command->add_option("-i, --in", in_, "ciphertext binary")->required();
+	command->add_option("-o, --out", out_, "plaintext binary")->required();
+	command->callback([this]() { run(); });
+	map_.insert(std::make_pair<std::string, rsa::attack>("fermat", rsa::attack::fermat));
+	map_.insert(std::make_pair<std::string, rsa::attack>("pollards-rho", rsa::attack::pollards_rho));
+	map_.insert(std::make_pair<std::string, rsa::attack>("pollards-p1", rsa::attack::pollards_p1));
+	map_.insert(std::make_pair<std::string, rsa::attack>("williams-p1", rsa::attack::williams_p1));
+}
+
+void command_rsa_attack::run() {
+	if (map_.find(method_) != map_.end()) {
+		decryption_rsa engine;
+		std::string public_key;
+		if (read_text(public_pem_, public_key) == 0) {
+			if (engine.calckey(public_key, map_[method_]) == 0) {
+				std::vector<uint8_t> cipher;
+				std::vector<uint8_t> plain;
+				if (read_binary(in_, cipher) == 0) {
+					if (engine.decrypt(cipher, plain) == 0) {
+						if (write_binary(out_, plain) == 0) {
+							std::cout << engine.base64(plain) << std::endl;
+							return;
+						}
 					}
 				}
 			}
