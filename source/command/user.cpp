f079cd9946380ebee97f84663018018158c0bad6
metrovian <paramedoct@gmail.com>
2025-09-11 23:57:43 +0900

    ADD. [MAIN] - 명령줄 인터페이스 비선형 최적화 파라미터 설정 구현

--- source/command/user.cpp
+++ source/command/user.cpp
@@ -8,6 +8,7 @@ void command_user::setup(CLI::App *parent) {
 	command->add_option("-i, --in", in_, "measured csv")->required();
 	command->add_option("-n, --iter", iter_, "iteration max")->default_val<size_t>(1000);
 	command->add_option("-e, --eps", eps_, "iteration eps")->default_val<double>(1.000E-15);
+	command->add_option("-p, --params", params_, "parameters");
 	command->callback([this]() { run(); });
 	return;
 }
@@ -17,6 +18,12 @@ void command_user::run() {
 	if (engine.import_function(function_).length() > 0) {
 		Eigen::VectorXd domain;
 		Eigen::VectorXd range;
+		Eigen::VectorXd params = engine.export_parameters();
+		for (size_t i = 0; i < std::min(static_cast<size_t>(params.size()), params_.size()); ++i) {
+			params[i] = params_[i];
+		}
+
+		engine.import_parameters(params);
 		if (read_vector(in_, domain, range, ',') == 0) {
 			if (engine.calibrate(domain, range, iter_, eps_).norm() >= 0) {
 				std::cout << engine.export_parameters().transpose() << std::endl;
