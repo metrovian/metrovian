702e7f804b7c741347dff8045af5ded702fe2035
metrovian <paramedoct@gmail.com>
2025-09-21 22:26:21 +0900

    CHANGE. [OPTIMIZE] - 비선형 최적화 모델을 확인할 수 있도록 변경

--- source/command/optimization/polynomial.cpp
+++ source/command/optimization/polynomial.cpp
@@ -5,7 +5,7 @@
 void command_polynomial::setup(CLI::App *parent) {
 	auto command = parent->add_subcommand("polynomial", "POLYNOMIAL optimization")->group("OPTIMIZATION");
 	command->add_option("-f, --func", model_, "polynomial-approximated function")->required();
-	command->add_option("-i, --in", in_, "measured csv")->required();
+	command->add_option("-i, --in", in_, "measured csv");
 	command->add_option("-n, --iter", iter_, "iteration max")->default_val<size_t>(1000);
 	command->add_option("-e, --eps", eps_, "iteration epsilon")->default_val<double>(1.000E-15);
 	command->add_option("-p, --params", params_, "parameters");
@@ -28,6 +28,8 @@ void command_polynomial::run() {
 			if (engine.calibrate(domain, range, iter_, eps_).norm() >= 0) {
 				std::cout << engine.export_parameters().transpose() << std::endl;
 			}
+		} else {
+			std::cout << engine.export_function() << std::endl;
 		}
 	}
 
