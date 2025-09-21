64b1bfe226d10f4bbdde6550ef57d6c958186754
metrovian <paramedoct@gmail.com>
2025-09-21 13:04:29 +0900

    ADD. [COMMON] - GSL 라이브러리 빌드 스크립트 적용

--- 3rdparty/setup-linux.sh
+++ 3rdparty/setup-linux.sh
@@ -3,3 +3,15 @@
 git submodule foreach --recursive "git reset --hard"
 git submodule foreach --recursive "git clean -fd"
 git submodule update --init --recursive
+
+sudo apt update
+sudo apt install \
+        libssl-dev \
+        libgsl-dev \
+        libgmp-dev \
+        libasound2-dev \
+        libavcodec-dev \
+        libavformat-dev \
+        libavutil-dev \
+        libswscale-dev \
+        libswresample-dev \
