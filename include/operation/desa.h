32224f6e43352192d6ee11be4dc35def48faef58
metrovian <paramedoct@gmail.com>
2025-09-24 23:53:28 +0900

    ADD. [OPERATE] - DESA-1 에너지 분리 연산자 구현

--- include/operation/desa.h
+++ include/operation/desa.h
@@ -6,19 +6,19 @@ protected: /* energy operator */
 	Eigen::VectorXd tkeo(const Eigen::VectorXd &range);
 
 public: /* overload */
-	int8_t operate(const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency);
-	int8_t operate(const Eigen::VectorXd &range, Eigen::VectorXd &amplitude);
+	int8_t operate(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency);
+	int8_t operate(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude);
 
 protected: /* operation */
-	virtual int8_t operation(const Eigen::VectorXd &range) = 0;
+	virtual int8_t operation(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency) = 0;
 };
 
 class operation_desa1 : public operation_desa {
 protected: /* operation */
-	virtual int8_t operation(const Eigen::VectorXd &range);
+	virtual int8_t operation(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency);
 };
 
 class operation_desa2 : public operation_desa {
 protected: /* operation */
-	virtual int8_t operation(const Eigen::VectorXd &range);
+	virtual int8_t operation(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency);
 };
--- source/operation/desa.cpp
+++ source/operation/desa.cpp
@@ -2,7 +2,7 @@
 
 Eigen::VectorXd operation_desa::tkeo(const Eigen::VectorXd &range) {
 	Eigen::VectorXd psi = range;
-	for (size_t i = 0; i < psi.size(); ++i) {
+	for (size_t i = 0; i < range.size(); ++i) {
 		psi[i] *= psi[i];
 		if (i == psi.size() - 1) {
 			psi[i] -= range[0] * range[i + 1];
@@ -14,4 +14,37 @@ Eigen::VectorXd operation_desa::tkeo(const Eigen::VectorXd &range) {
 	}
 
 	return psi;
+}
+
+int8_t operation_desa::operate(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency) {
+	return operation(domain, range, amplitude, frequency);
+}
+
+int8_t operation_desa::operate(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude) {
+	Eigen::VectorXd frequency;
+	return operation(domain, range, amplitude, frequency);
+}
+
+int8_t operation_desa1::operation(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency) {
+	Eigen::VectorXd prev = Eigen::VectorXd::Zero(range.size());
+	for (size_t i = 1; i < range.size(); ++i) {
+		prev[i] = range[i - 1];
+	}
+
+	amplitude = Eigen::VectorXd::Zero(range.size());
+	frequency = Eigen::VectorXd::Zero(range.size());
+	Eigen::VectorXd psi = tkeo(range);
+	Eigen::VectorXd dsi = tkeo(range - prev);
+	for (size_t i = 1; i < range.size(); ++i) {
+		if (domain[i] != domain[i - 1]) {
+			amplitude[i] = psi[i] / std::sqrt(dsi[i]);
+			frequency[i] = std::sqrt(dsi[i] / psi[i]) / (domain[i] - domain[i - 1]);
+		}
+	}
+
+	return 0;
+}
+
+int8_t operation_desa2::operation(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &amplitude, Eigen::VectorXd &frequency) {
+	return 0;
 }
