aec638787eaec726bf6ea9ae1442a00f57eb21c6
metrovian <paramedoct@gmail.com>
2025-09-29 22:37:02 +0900

    CHANGE. [OPTIMIZE] - 통일성을 위해 조화진동자 명칭 변경

--- include/command/optimization/sinusoidal.h
+++ include/command/optimization/sinusoidal.h
@@ -2,13 +2,13 @@
 #include "command/abstract.h"
 #include "command/optimization/user.h"
 
-namespace oscillator {
+namespace sinusoidal {
 enum class function : uint8_t;
 };
 
-class command_oscillator : public command_user {
+class command_sinusoidal : public command_user {
 protected: /* parameter */
-	std::unordered_map<std::string, oscillator::function> map_;
+	std::unordered_map<std::string, sinusoidal::function> map_;
 
 public: /* abstract */
 	virtual void setup(CLI::App *parent) override final;
