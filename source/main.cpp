a8380c49716fcedf2045ad5194ca01bb04fe944d
metrovian <paramedoct@gmail.com>
2025-10-03 19:52:30 +0900

    DELETE. [MAIN] - 정책 변경으로 AES 계열 명령어 제거

--- source/main.cpp
+++ source/main.cpp
@@ -54,9 +54,6 @@ int32_t main(int argc, char **argv) {
 	interface_singleton::instance().command_setup(std::make_unique<command_sinusoidal>());
 	interface_singleton::instance().command_setup(std::make_unique<command_transcendental>());
 	interface_singleton::instance().command_setup(std::make_unique<command_user>());
-	interface_singleton::instance().command_setup(std::make_unique<command_aes256_cbc>());
-	interface_singleton::instance().command_setup(std::make_unique<command_aes256_ctr>());
-	interface_singleton::instance().command_setup(std::make_unique<command_aes256_gcm>());
 	interface_singleton::instance().command_setup(std::make_unique<command_rsa>());
 	interface_singleton::instance().command_setup(std::make_unique<command_ecdh>());
 	interface_singleton::instance().command_setup(std::make_unique<command_avcodec>());
