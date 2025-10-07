8e4ca9a2a108710df7e40e16f7e2f735f93ba7e5
metrovian <paramedoct@gmail.com>
2025-10-07 19:49:47 +0900

    ADD. [DECRYPT] - ECDH 공격 알고리즘 구성 준비

--- include/reconstruction/decryption/ecdh.h
+++ include/reconstruction/decryption/ecdh.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "reconstruction/decryption/abstract.h"
 
+namespace ecdh {
+enum class attack : uint8_t {
+	trial = 0,
+};
+};
+
 class decryption_ecdh : public decryption_abstract {
 protected: /* parameter */
 	std::vector<uint8_t> private_key_;
@@ -9,6 +15,10 @@ protected: /* parameter */
 public: /* setter */
 	int8_t setkey(const std::vector<uint8_t> &private_key);
 	int8_t setkey(const std::string &private_key);
+
+public: /* attack */
+	int8_t calckey(const std::vector<uint8_t> &public_key, ecdh::attack algorithm);
+	int8_t calckey(const std::string &public_key, ecdh::attack algorithm);
 
 protected: /* abstract */
 	virtual int8_t decryption(const std::vector<uint8_t> &public_key, std::vector<uint8_t> &shared_key) override final;
