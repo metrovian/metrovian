0ff0e2c196d8fb9be007f5008d6bbebae15e8b54
metrovian <paramedoct@gmail.com>
2025-10-08 15:15:19 +0900

    ADD. [DECRYPT] - ECDH 타원곡선 대수 연산자 구현 - ECDH 트라이얼 공격 알고리즘 구현

--- source/command/decryption/ecdh.cpp
+++ source/command/decryption/ecdh.cpp
@@ -3,7 +3,23 @@
 #include "predefined.h"
 
 void command_ecdh::setup(CLI::App *parent) {
-	auto command = parent->add_subcommand("ecdh", "ECDH decryption")->group("DECRYPTION");
+	command_parser_ = parent->add_subcommand("ecdh", "ECDH decryption")->group("DECRYPTION");
+	command_parser_->callback([this]() { run(); });
+	setup_subcommand(std::make_unique<command_ecdh_private>());
+	setup_subcommand(std::make_unique<command_ecdh_public>());
+	return;
+}
+
+void command_ecdh::run() {
+	if (select_subcommand() == 0) {
+		throw CLI::CallForHelp();
+	}
+
+	return;
+}
+
+void command_ecdh_private::setup(CLI::App *parent) {
+	auto command = parent->add_subcommand("private", "ECDH decryption")->group("DECRYPTION");
 	command->add_option("-p, --pem", private_pem_, "private pem")->required();
 	command->add_option("-i, --in", in_, "ciphertext binary")->required();
 	command->add_option("-o, --out", out_, "plaintext binary")->required();
@@ -11,7 +27,7 @@ void command_ecdh::setup(CLI::App *parent) {
 	return;
 }
 
-void command_ecdh::run() {
+void command_ecdh_private::run() {
 	decryption_ecdh engine;
 	std::string private_key;
 	if (read_text(private_pem_, private_key) == 0) {
@@ -24,6 +40,45 @@ void command_ecdh::run() {
 						std::cout << engine.base64(plain) << std::endl;
 						return;
 					}
+				}
+			}
+		}
+	}
+}
+
+void command_ecdh_public::setup(CLI::App *parent) {
+	auto command = parent->add_subcommand("public", "ECDH-EXPLOIT decryption");
+	command->add_option("-p, --pem", public_pem_, "public pem")->required();
+	command->add_option("-m, --method", method_, "attack method")->required();
+	command->add_option("-i, --in", in_, "ciphertext binary");
+	command->add_option("-o, --out", out_, "plaintext binary")->required();
+	command->callback([this]() { run(); });
+	map_.insert(std::make_pair<std::string, ecdh::attack>("trial", ecdh::attack::trial));
+}
+
+void command_ecdh_public::run() {
+	if (map_.find(method_) != map_.end()) {
+		decryption_ecdh engine;
+		std::string public_key;
+		if (read_text(public_pem_, public_key) == 0) {
+			if (engine.calckey(public_key, map_[method_]) == 0) {
+				std::vector<uint8_t> cipher;
+				std::vector<uint8_t> plain;
+				if (in_.length() > 0) {
+					if (read_binary(in_, cipher) == 0) {
+						if (engine.decrypt(cipher, plain) == 0) {
+							if (write_binary(out_, plain) == 0) {
+								std::cout << engine.base64(plain) << std::endl;
+								return;
+							}
+						}
+					}
+				} else {
+					// std::string pem = engine.pem();
+					// if (write_text(out_, pem) == 0) {
+					// 	std::cout << pem << std::endl;
+					// 	return;
+					// }
 				}
 			}
 		}
