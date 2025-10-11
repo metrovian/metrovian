2243b141552cf5a2d6230d6c51f54ebd538ff935
metrovian <paramedoct@gmail.com>
2025-10-11 13:46:26 +0900

    DELETE. [COMMON] - 빌드시스템 전환을 위해 서드파티 패키지 통합

--- 3rdparty/setup-debian.sh
+++ 3rdparty/setup-debian.sh
@@ -4,10 +4,15 @@ git submodule foreach --recursive "git clean -fd"
 git submodule update --init --recursive
 sudo apt update
 sudo apt install \
+        nlohmann-json3-dev \
+        libasound2-dev \
+        libcli11-dev \
+        libeigen3-dev \
+        libmuparser-dev \
+        libspdlog-dev \
         libssl-dev \
         libgsl-dev \
         libgmp-dev \
-        libasound2-dev \
         libavcodec-dev \
         libavformat-dev \
         libavutil-dev \
