0d0c563bda254a3d660b697504fd90d4424ea14f
metrovian <paramedoct@gmail.com>
2025-10-12 15:45:43 +0900

    ADD. [OPERATE] - Cooley-Tukey 고속 역푸리에 연산자 구현

--- core/include/operation/fft.h
+++ core/include/operation/fft.h
@@ -6,16 +6,26 @@ protected: /* fourier operator */
 	Eigen::VectorXcd fft(const Eigen::VectorXcd &range);
 
 public: /* overload */
-	int8_t operate(
-	    const Eigen::VectorXd &domain,
-	    const Eigen::VectorXd &range,
-	    Eigen::VectorXd &frequency,
-	    Eigen::VectorXd &spectrum);
+	int8_t operate(const Eigen::VectorXd &domain, const Eigen::VectorXcd &range, Eigen::VectorXd &frequency, Eigen::VectorXcd &spectrum);
+	int8_t operate(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &frequency, Eigen::VectorXcd &spectrum);
+	int8_t operate(const Eigen::VectorXd &domain, const Eigen::VectorXd &range, Eigen::VectorXd &frequency, Eigen::VectorXd &spectrum);
 
 protected: /* operation */
-	int8_t operation(
+	virtual int8_t operation(
 	    const Eigen::VectorXd &domain,
-	    const Eigen::VectorXd &range,
+	    const Eigen::VectorXcd &range,
 	    Eigen::VectorXd &frequency,
-	    Eigen::VectorXd &spectrum);
+	    Eigen::VectorXcd &spectrum);
+};
+
+class operation_ifft : public operation_fft {
+protected: /* fourier operator */
+	Eigen::VectorXcd ifft(const Eigen::VectorXcd &spectrum);
+
+protected: /* operation */
+	virtual int8_t operation(
+	    const Eigen::VectorXd &frequency,
+	    const Eigen::VectorXcd &spectrum,
+	    Eigen::VectorXd &domain,
+	    Eigen::VectorXcd &range);
 };
