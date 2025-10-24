0f537b9db3ac179b8acf291764292376f9340f54
metrovian <paramedoct@gmail.com>
2025-10-24 22:52:00 +0900

    CHANGE. [DECOMP] - 오디오 파라미터 정의 위치 변경

--- core/include/reconstruction/decompression/audio.h
+++ core/include/reconstruction/decompression/audio.h
@@ -2,6 +2,10 @@
 #include "reconstruction/decompression/abstract.h"
 
 class decompression_audio : public decompression_abstract {
+protected: /* parameter */
+	uint16_t channels_ = 0;
+	uint32_t sample_rate_ = 0;
+
 public: /* overload */
 	virtual void decompress(const std::string &path) override final;
 	virtual void decompress(const std::string &path, const std::string &record) override final;
