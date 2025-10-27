ed8b457b1618ee7154efe7ed260b10622f3834ab
metrovian <paramedoct@gmail.com>
2025-10-27 22:13:25 +0900

    CHANGE. [PROPERTY] - 프로퍼티 단축

--- core/include/predefined.h
+++ core/include/predefined.h
@@ -7,5 +7,11 @@
 #define LOG_CONDITION(condition)        spdlog::error("[condition] [{}:{}] {}", basename(__FILE__), __LINE__, #condition)
 #define LOG_ARGUMENT(argument)          spdlog::critical("[argument] [{}:{}] {}", basename(__FILE__), __LINE__, #argument)
 
+#define CONFIG_STRING(...)              property_singleton::instance().parse(std::vector<std::string>{__VA_ARGS__})
+#define CONFIG_INT32(...)               std::stoi(CONFIG_STRING(__VA_ARGS__))
+#define CONFIG_INT64(...)               std::stoll(CONFIG_STRING(__VA_ARGS__))
+#define CONFIG_UINT32(...)              std::stoul(CONFIG_STRING(__VA_ARGS__))
+#define CONFIG_UINT64(...)              std::stoull(CONFIG_STRING(__VA_ARGS__))
+
 #define PATH_PROPERTY                   "/.config/metrovian.json"
 // clang-format on
