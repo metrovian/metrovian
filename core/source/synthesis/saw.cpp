e8a99b72a3afaca15eb1d0c40e66ce9e12d6fe51
metrovian <paramedoct@gmail.com>
2025-10-27 16:42:05 +0900

    ADD. [SYNTH] - 파형합성 톱니파 클래스 구현

--- core/source/synthesis/saw.cpp
+++ core/source/synthesis/saw.cpp
@@ -1,8 +1,8 @@
-#include "synthesis/sin.h"
+#include "synthesis/saw.h"
 #include "property.h"
 #include "predefined.h"
 
-void synthesis_sin::synthesis(uint64_t note, uint64_t period) {
+void synthesis_saw::synthesis(uint64_t note, uint64_t period) {
 	LOG_ENTER();
 	uint64_t sample_rate = std::stoull(property_singleton::instance().parse({"synthesis", "sample-rate"}));
 	std::vector<int16_t> sample(sample_rate * period, 0);
@@ -12,7 +12,11 @@ void synthesis_sin::synthesis(uint64_t note, uint64_t period) {
 			double time = static_cast<double>(j) / sample_rate;
 			double pitch = std::pow(2.000E+0, (static_cast<int64_t>(i) - 69) / 1.200E+1) * 4.400E+2;
 			double phase = std::fmod(pitch * time, 1.000E+0);
-			sample[j] = static_cast<int16_t>(std::sin(phase * (M_PI * 2.000E+0)) * 3276);
+			if (phase < skew_) {
+				sample[j] = ((phase / skew_) * 2.000E+0 - 1.000E+0) * 3276;
+			} else {
+				sample[j] = ((1.000E+0 - (phase - skew_) / (1.000E+0 - skew_)) * 2.000E+0 - 1.000E+0) * 3276;
+			}
 		}
 
 		muxer_.keysample(sample, i);
--- core/source/synthesis/square.cpp
+++ core/source/synthesis/square.cpp
@@ -9,9 +9,10 @@ void synthesis_square::synthesis(uint64_t note, uint64_t period) {
 	muxer_.keysize(note);
 	for (uint64_t i = 0; i < note; ++i) {
 		for (uint64_t j = 0; j < sample.size(); ++j) {
-			double pitch = std::pow(2.000E+0, (static_cast<int64_t>(i) - 69) / 1.200E+1) * 4.400E+2;
 			double time = static_cast<double>(j) / sample_rate;
-			if (std::fmod(pitch * time, 1.000E+0) < duty_) {
+			double pitch = std::pow(2.000E+0, (static_cast<int64_t>(i) - 69) / 1.200E+1) * 4.400E+2;
+			double phase = std::fmod(pitch * time, 1.000E+0);
+			if (phase < duty_) {
 				sample[j] = 3276;
 			} else {
 				sample[j] = -3276;
