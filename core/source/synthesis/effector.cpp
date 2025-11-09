1f53e4e20f80841ff1bdbcc39ed78823fb465533
metrovian <paramedoct@gmail.com>
2025-11-09 19:21:35 +0900

    CHANGE. [SYNTH] - 피치시프트 파라미터 자료형 변경

--- core/source/synthesis/effector.cpp
+++ core/source/synthesis/effector.cpp
@@ -1,15 +1,11 @@
 #include "synthesis/effector.h"
 
-double synthesis_effector::note(uint64_t note, int64_t cent) {
-	return note + static_cast<double>(cent) / 1.000E+2;
-}
-
 double synthesis_effector::shift(
     const std::function<double(uint64_t, uint64_t)> waveform,
+    double cent,
     uint64_t note,
-    uint64_t sample,
-    int64_t cent) {
-	double ratio = std::pow(2.000E+0, static_cast<double>(cent) / 1.200E+3);
+    uint64_t sample) {
+	double ratio = std::pow(2.000E+0, cent / 1.200E+3);
 	double pos = static_cast<double>(sample) * ratio;
 	double frac = pos - std::floor(pos);
 	double sample1 = waveform(note, std::floor(pos));
