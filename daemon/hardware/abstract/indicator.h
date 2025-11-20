5b3decf91a47673ade05804c4d71c08c5d95c861
metrovian <paramedoct@gmail.com>
2025-11-20 22:09:31 +0900

    ADD. [MAIN] - Hardware Abstract: Rotary Encoder 구현

--- daemon/hardware/abstract/indicator.h
+++ daemon/hardware/abstract/indicator.h
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <memory>
 
 namespace indicator {
 enum class state : uint8_t {
