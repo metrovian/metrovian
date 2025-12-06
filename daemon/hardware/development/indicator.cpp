e98f9088d0eec2da02e2a0b1eafa41c07f0f3ef3
metrovian <paramedoct@gmail.com>
2025-12-06 14:16:29 +0900

    DELETE. [MAIN] - Hardware 관련 구현내용 제거

--- daemon/hardware/development/indicator.cpp
+++
@@ -1,7 +0,0 @@
-#include "daemon/hardware/development/indicator.h"
-#include "core/predefined.h"
-
-void indicator_development::set(indicator::state state) {
-	LOG_VALUE(static_cast<int32_t>(state));
-	return;
-}
