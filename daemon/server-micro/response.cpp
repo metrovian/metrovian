4c8d0fa2f15bfb37fc72b08e13d3356f12103c5a
metrovian <paramedoct@gmail.com>
2025-12-24 22:28:04 +0900

    CHANGE. [MAIN] - 프리셋 기반으로 웹 컨트롤러 구조 개선

--- daemon/server-micro/response.cpp
+++ daemon/server-micro/response.cpp
@@ -20,7 +20,7 @@ MHD_Result response::empty(struct MHD_Connection *connection, uint32_t code) {
 	return ret;
 }
 
-MHD_Result response::numeric(MHD_Connection *connection, uint32_t value) {
+MHD_Result response::numeric(struct MHD_Connection *connection, uint32_t value) {
 	std::string str_value = std::to_string(value);
 	struct MHD_Response *response =
 	    MHD_create_response_from_buffer(
@@ -35,6 +35,28 @@ MHD_Result response::numeric(MHD_Connection *connection, uint32_t value) {
 		ret = MHD_add_response_header(response, "Pragma", "no-cache");
 		ret = MHD_add_response_header(response, "Expires", "0");
 		ret = MHD_add_response_header(response, "Access-Control-Allow-Origin", "*");
+		ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+		MHD_destroy_response(response);
+	}
+
+	return ret;
+}
+
+MHD_Result response::json(struct MHD_Connection *connection, const nlohmann::json &object) {
+	std::string str_object = object.dump();
+	struct MHD_Response *response =
+	    MHD_create_response_from_buffer(
+		str_object.size(),
+		(void *)str_object.c_str(),
+		MHD_RESPMEM_MUST_COPY);
+
+	MHD_Result ret = MHD_Result::MHD_NO;
+	if (response != nullptr) {
+		MHD_add_response_header(response, "Content-Type", "application/json");
+		MHD_add_response_header(response, "Cache-Control", "no-store, no-cache, must-revalidate");
+		MHD_add_response_header(response, "Pragma", "no-cache");
+		MHD_add_response_header(response, "Expires", "0");
+		MHD_add_response_header(response, "Access-Control-Allow-Origin", "*");
 		ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
 		MHD_destroy_response(response);
 	}
--- daemon/server-micro/response.h
+++ daemon/server-micro/response.h
@@ -11,9 +11,11 @@
 #include <unistd.h>
 #include <spdlog/spdlog.h>
 #include <microhttpd.h>
+#include <nlohmann/json.hpp>
 
 class response {
 public: /* export */
 	static MHD_Result empty(struct MHD_Connection *connection, uint32_t code);
 	static MHD_Result numeric(struct MHD_Connection *connection, uint32_t value);
+	static MHD_Result json(struct MHD_Connection *connection, const nlohmann::json &object);
 };
