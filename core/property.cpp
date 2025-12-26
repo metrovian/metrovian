44859e0934662587564287ec92e9f2678f78e33e
metrovian <paramedoct@gmail.com>
2025-12-26 15:03:13 +0900

    CHANGE. [PROPERTY] - 디폴트 설정 불러오기 시점 변경

--- core/property.cpp
+++ core/property.cpp
@@ -63,7 +63,6 @@ void property_singleton::merge_default(nlohmann::ordered_json &target, const nlo
 }
 
 property_singleton::property_singleton() {
-	load_default();
 	std::string dir = std::string(std::getenv("HOME")) + PATH_DIR;
 	if (access(dir.c_str(), F_OK) != 0) {
 		if (mkdir(dir.c_str(), 0755) != 0) {
@@ -72,6 +71,7 @@ property_singleton::property_singleton() {
 		}
 	}
 
+	load_default();
 	std::string path = std::string(std::getenv("HOME")) + PATH_PROPERTY;
 	std::ifstream ifs(path);
 	if (ifs.is_open() == true) {
