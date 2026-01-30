01041138464d0ec9deef3f23ce0e733a190b4fe4
metrovian <paramedoct@gmail.com>
2026-01-30 22:52:14 +0900

    ADD. [SOUND] - MIDI Automata 자동연주 동작 구현

--- core/sound/automata.h
+++ core/sound/automata.h
@@ -3,11 +3,26 @@
 
 extern "C" {
 #include <alsa/asoundlib.h>
+#include <smf.h>
 }
 
 class automata_singleton {
 protected: /* handle */
 	snd_seq_t *handle_ = nullptr;
+	smf_t *smf_ = nullptr;
+
+protected: /* control */
+	std::atomic<uint8_t> state_ = 0;
+	int port_ = 0;
+	const int noteon_ = 0x90;
+	const int noteoff_ = 0x80;
+
+protected: /* thread */
+	void thread_playback();
+
+public: /* export */
+	int8_t open(std::string name);
+	int8_t close();
 
 public: /* instance */
 	static automata_singleton &instance();
