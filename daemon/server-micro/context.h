5d50997037a50998be8d3bc380ab59c36f601da3
metrovian <paramedoct@gmail.com>
2026-01-31 14:39:07 +0900

    FIXED. [MAIN] - 브라우저를 닫아도 프리셋 선택 정보를 유지

--- daemon/server-micro/context.h
+++ daemon/server-micro/context.h
@@ -4,13 +4,15 @@
 class context_singleton {
 protected: /* context */
 	machine::state state_ = machine::state::setup;
-	nlohmann::ordered_json preset_;
 	nlohmann::ordered_json presets_;
+	nlohmann::ordered_json preset_;
+	uint8_t id_ = 0;
 	uint64_t progress_ = 0;
 
 public: /* api getter */
 	machine::state apiget_state();
 	nlohmann::ordered_json apiget_presets();
+	uint8_t apiget_preset();
 	uint64_t apiget_progress();
 	uint64_t apiget_notes();
 
--- daemon/server-micro/parser/read/waveform.cpp
+++ daemon/server-micro/parser/read/waveform.cpp
@@ -3,7 +3,7 @@
 #include "daemon/server-micro/response.h"
 
 MHD_Result read_waveform::parse(MHD_Connection *connection) {
-	return response::json(
+	return response::numeric(
 	    connection,
-	    context_singleton::instance().apiget_presets());
+	    context_singleton::instance().apiget_preset());
 }
