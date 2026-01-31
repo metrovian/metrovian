94829c41762b1088bb48bbde76d90373364fd4e8
metrovian <paramedoct@gmail.com>
2026-01-31 17:43:22 +0900

    ADD. [MAIN] - 웹 제어패널 UI/UX 개선

--- system/nginx/html/script/setup.js
+++ system/nginx/html/script/setup.js
@@ -1,68 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const STATE_UI = {
-    1: { label: "SETUP", class: "setup" },
-    2: { label: "SYNTH", class: "synthesis" },
-    3: { label: "PLAY", class: "perform" },
-    400: { label: "ERROR", class: "error" },
-  };
+  window.addEventListener("changeState", async (event) => {
+    const STATE_UI = {
+      1: {
+        waveSelect1: false,
+        waveSelect2: true,
+        waveConfirm: false,
+      },
+      3: {
+        waveSelect1: true,
+        waveSelect2: false,
+        waveConfirm: false,
+      },
+    };
 
-  const stateCircle = document.getElementById("stateCircle");
-  const stateText = document.getElementById("stateText");
-  const waveSetup = document.getElementById("waveSetup");
+    const STATE_DEFAULT = {
+      waveSelect1: true,
+      waveSelect2: true,
+      waveConfirm: true,
+    };
+
+    const ui = STATE_UI[event.detail] ?? STATE_DEFAULT;
+    waveSelect1.disabled = ui.waveSelect1;
+    waveSelect2.disabled = ui.waveSelect2;
+    waveConfirm.disabled = ui.waveConfirm;
+    waveSelect1.classList.toggle("disabled", ui.waveSelect1);
+    waveSelect2.classList.toggle("disabled", ui.waveSelect2);
+    waveConfirm.classList.toggle("disabled", ui.waveConfirm);
+
+    if (event.detail == 2 || event.detail == 3) {
+      const select = await asyncAPI({ action: "read", waveform: 0 });
+      const id = String(select);
+      waveSelect1.value = id;
+    }
+
+    return;
+  });
+
   const waveSelect1 = document.getElementById("waveSelect1");
   const waveSelect2 = document.getElementById("waveSelect2");
   const waveConfirm = document.getElementById("waveConfirm");
-
-  function applyState(state) {
-    const info = STATE_UI[state];
-    stateText.textContent = info.label;
-    stateCircle.classList.remove(
-      ...Object.values(STATE_UI).map((v) => v.class)
-    );
-
-    stateCircle.classList.add(info.class);
-    if (info.label == "SETUP") {
-      waveSelect1.disabled = false;
-      waveSelect2.disabled = true;
-      waveConfirm.disabled = false;
-      waveSetup.classList.remove("disabled");
-    } else if (info.label == "PLAY") {
-      waveSelect1.disabled = true;
-      waveSelect2.disabled = false;
-      waveConfirm.disabled = false;
-      waveSetup.classList.remove("disabled");
-    } else {
-      waveSelect1.disabled = true;
-      waveSelect2.disabled = true;
-      waveConfirm.disabled = true;
-      waveSetup.classList.add("disabled");
-    }
-
-    return;
-  }
-
-  async function updateState() {
-    try {
-      const raw = await asyncAPI({ action: "read", state: 0 });
-      const state = raw.trim();
-      applyState(state);
-      if (state == 2) {
-        const now = await asyncAPI({ action: "read", progress: 0 });
-        const max = await asyncAPI({ action: "read", notes: 0 });
-        stateCircle.style.setProperty(
-          "--progress",
-          (parseInt(now) * 100.0) / parseInt(max)
-        );
-      } else {
-        stateCircle.style.removeProperty("--progress");
-      }
-    } catch (err) {
-      applyState(400);
-      console.error("State", err);
-    }
-
-    return;
-  }
 
   async function updateOptions() {
     const res = await asyncAPI({ action: "read", waveforms: 0 });
@@ -73,12 +49,6 @@ document.addEventListener("DOMContentLoaded", () => {
       opt.textContent = wave.name;
       waveSelect1.appendChild(opt);
     });
-
-    if (stateText.textContent == "SYNTH" || stateText.textContent == "PLAY") {
-      const select = await asyncAPI({ action: "read", waveform: 0 });
-      const id = String(select);
-      waveSelect1.value = id;
-    }
 
     return;
   }
@@ -111,8 +81,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  updateState();
   updateOptions();
-  setInterval(updateState, 200);
   return;
 });
