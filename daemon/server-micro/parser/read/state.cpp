7aec816ae64df618a02c754423f1e0a1d870fd74
metrovian <paramedoct@gmail.com>
2026-02-04 23:00:59 +0900

    ADD. [MAIN] - MIDI Automata Play/State API 구현

--- daemon/server-micro/parser/read/state.cpp
+++ daemon/server-micro/parser/read/state.cpp
@@ -1,9 +1,13 @@
 #include "daemon/server-micro/parser/read/state.h"
+#include "daemon/server-micro/automata.h"
 #include "daemon/server-micro/context.h"
 #include "daemon/server-micro/response.h"
 
 MHD_Result read_state::parse(MHD_Connection *connection) {
-	return response::numeric(
-	    connection,
-	    static_cast<uint32_t>(context_singleton::instance().apiget_state()));
+	uint32_t state = static_cast<uint32_t>(context_singleton::instance().apiget_state());
+	if (state == static_cast<uint32_t>(machine::state::performance)) {
+		state += static_cast<uint32_t>(automata_singleton::instance().get_state());
+	}
+
+	return response::numeric(connection, state);
 }
