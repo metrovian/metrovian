adc95acacbfca34e9f084a65b787aa78d2e841f4
metrovian <paramedoct@gmail.com>
2026-02-15 14:10:26 +0900

    DELETE. [MAIN] - 불필요한 헤더 제거

--- daemon/server-micro/response.h
+++ daemon/server-micro/response.h
@@ -1,22 +1,18 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <iostream>
-#include <vector>
-#include <map>
-#include <memory>
-#include <atomic>
-#include <thread>
-#include <unistd.h>
-#include <spdlog/spdlog.h>
+#include <string>
 #include <microhttpd.h>
 #include <nlohmann/json.hpp>
 
 class response {
-public: /* export */
+public:
 	static MHD_Result empty(struct MHD_Connection *connection, uint32_t code);
 	static MHD_Result numeric(struct MHD_Connection *connection, uint32_t value);
 	static MHD_Result string(struct MHD_Connection *connection, std::string value);
 	static MHD_Result json(struct MHD_Connection *connection, const nlohmann::ordered_json &object);
+
+private:
+	static MHD_Result queue_response(
+	    struct MHD_Connection *connection,
+	    struct MHD_Response *response,
+	    uint32_t code);
 };
