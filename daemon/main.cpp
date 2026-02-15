f2ffd3ab6ee2912b4504d18264ab4e70344b7ca1
metrovian <paramedoct@gmail.com>
2026-02-15 12:10:34 +0900

    FORMAT. [MAIN] - 접근 제한자 영역 재배치

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -2,54 +2,6 @@
 #include "core/property.h"
 #include "core/predefined.h"
 
-void machine_singleton::setup(const nlohmann::ordered_json &preset) {
-	LOG_ENTER();
-	std::string method = preset.value("method", "");
-	if (method == std::string("add")) {
-		core_ = std::make_unique<synthesis_add>(preset);
-	}
-
-	LOG_EXIT();
-	return;
-}
-
-void machine_singleton::setup() {
-	core_.reset();
-	return;
-}
-
-void machine_singleton::synthesize() {
-	core_->callback_synthesis([&](uint64_t value) {
-		context_singleton::instance().set_progress(value);
-	});
-
-	core_->synthesize();
-	return;
-}
-
-void machine_singleton::perform() {
-	core_->perform();
-	return;
-}
-
-void machine_singleton::handle_setup(const std::function<void(void)> handler) {
-	handler_ = handler;
-	if (handler_ != nullptr) {
-		std::signal(SIGTERM, machine_singleton::handle_terminate);
-		std::signal(SIGINT, machine_singleton::handle_terminate);
-	}
-
-	return;
-}
-
-void machine_singleton::handle_terminate(int) {
-	if (handler_ != nullptr) {
-		handler_();
-	}
-
-	return;
-}
-
 machine_singleton &machine_singleton::instance() {
 	static machine_singleton instance_;
 	return instance_;
@@ -84,35 +36,68 @@ void machine_singleton::panic() {
 	return;
 }
 
-void machine_singleton::load_stdout() {
-	std::cout << std::scientific << std::showpos << std::setprecision(6);
+void machine_singleton::handle_setup(const std::function<void(void)> handler) {
+	handler_ = handler;
+	if (handler_ != nullptr) {
+		std::signal(SIGTERM, machine_singleton::handle_terminate);
+		std::signal(SIGINT, machine_singleton::handle_terminate);
+	}
+
 	return;
 }
 
-void machine_singleton::load_stderr() {
+void machine_singleton::handle_terminate(int) {
+	if (handler_ != nullptr) {
+		handler_();
+	}
+
+	return;
+}
+
+machine_singleton::machine_singleton() {
+	std::cout << std::scientific << std::showpos << std::setprecision(6);
 	spdlog::set_default_logger(spdlog::stderr_color_mt("stderr"));
 	spdlog::set_level(spdlog::level::info);
-	return;
-}
-
-void machine_singleton::load_smap() {
 	smap_.insert(std::make_pair(machine::state::none, std::make_unique<state_none>()));
 	smap_.insert(std::make_pair(machine::state::shutdown, std::make_unique<state_none>()));
 	smap_.insert(std::make_pair(machine::state::setup, std::make_unique<state_setup>()));
 	smap_.insert(std::make_pair(machine::state::synthesis, std::make_unique<state_synthesis>()));
 	smap_.insert(std::make_pair(machine::state::performance, std::make_unique<state_performance>()));
-	return;
-}
-
-machine_singleton::machine_singleton() {
-	load_stdout();
-	load_stderr();
-	load_smap();
-	handle_setup([&]() { shutdown(); });
 	property_singleton::instance();
 	context_singleton::instance();
 	automata_singleton::instance();
 	api_singleton::instance();
+	handle_setup([&]() { shutdown(); });
+}
+
+void machine_singleton::setup(const nlohmann::ordered_json &preset) {
+	LOG_ENTER();
+	std::string method = preset.value("method", "");
+	if (method == std::string("add")) {
+		core_ = std::make_unique<synthesis_add>(preset);
+	}
+
+	LOG_EXIT();
+	return;
+}
+
+void machine_singleton::setup() {
+	core_.reset();
+	return;
+}
+
+void machine_singleton::synthesize() {
+	core_->callback_synthesis([&](uint64_t value) {
+		context_singleton::instance().set_progress(value);
+	});
+
+	core_->synthesize();
+	return;
+}
+
+void machine_singleton::perform() {
+	core_->perform();
+	return;
 }
 
 int main(int, char **) {
