91d2a075420e547fbdc7c822a257116df01ede2e
metrovian <paramedoct@gmail.com>
2026-02-16 23:30:14 +0900

    DELETE. [DECOMP] - 접근 제한자 영역 재배치 - 불필요한 헤더 제거

--- core/decompression/abstract.h
+++ core/decompression/abstract.h
@@ -1,24 +1,9 @@
 #pragma once
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <atomic>
-#include <condition_variable>
-#include <map>
-#include <mutex>
-#include <queue>
-#include <thread>
-#include <fstream>
-#include <sstream>
 #include <string>
-#include <stdint.h>
-#include <spdlog/spdlog.h>
 
 class decompression_abstract {
-public: /* overload */
+public:
+	virtual ~decompression_abstract() = default;
 	virtual void decompress(const std::string &path) = 0;
 	virtual void decompress(const std::string &path, const std::string &record) = 0;
-
-public: /* abstract */
-	virtual ~decompression_abstract() {}
 };
