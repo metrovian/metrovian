1284833b5b61e02255d6526a9ed295686ed95a6a
metrovian <paramedoct@gmail.com>
2026-02-16 23:39:52 +0900

    FORMAT. [CMD] - 접근 제한자 영역 재배치

--- command/operation/desa.h
+++ command/operation/desa.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "command/abstract.h"
 
-class command_desa : public command_abstract {
-protected: /* parameter */
+class command_desa final : public command_abstract {
+public:
+	void setup(CLI::App *parent) override;
+	void run() override;
+
+protected:
 	std::string in_;
 	std::string out_;
-
-public: /* abstract */
-	virtual void setup(CLI::App *parent) override;
-	virtual void run() override;
 };
