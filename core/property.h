99d9be0b02314a4fea3a7dbe858456e1e92ed8c5
metrovian <paramedoct@gmail.com>
2026-02-17 23:33:18 +0900

    FORMAT. [PROPERTY] - 접근 제한자 영역 재배치

--- core/property.h
+++ core/property.h
@@ -7,22 +7,23 @@
 #include <nlohmann/json.hpp>
 
 class property_singleton {
-protected: /* json parser */
-	nlohmann::ordered_json parser_;
-	nlohmann::ordered_json default_;
-
-public: /* json parser */
-	std::string parse(std::vector<std::string> property);
-
-public: /* instance */
+public:
 	static property_singleton &instance();
 
-private: /* migration */
+public:
+	property_singleton(const property_singleton &) = delete;
+	property_singleton(property_singleton &&) = delete;
+	property_singleton &operator=(const property_singleton &) = delete;
+	property_singleton &operator=(property_singleton &&) = delete;
+	std::string parse(std::vector<std::string> property);
+
+private:
+	~property_singleton() = default;
+	property_singleton();
 	void load_default();
 	void merge_default(nlohmann::ordered_json &target, const nlohmann::ordered_json &source);
 
-private: /* constraint */
-	property_singleton();
-	property_singleton(const property_singleton &) = default;
-	property_singleton &operator=(const property_singleton &) = default;
+private:
+	nlohmann::ordered_json parser_;
+	nlohmann::ordered_json default_;
 };
