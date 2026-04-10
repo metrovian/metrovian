1e4c7b202a01d9ac909950a579dbb221815d973f
metrovian <paramedoct@gmail.com>
2026-04-11 03:16:49 +0900

    ADD. [OPTIMIZE] - Horner scheme 적용하여 수치안정성 향상

--- core/optimization/polynomial.cpp
+++ core/optimization/polynomial.cpp
@@ -5,11 +5,11 @@ std::string optimization_polynomial::import_function(uint8_t model) {
 	std::ostringstream oss;
 	oss << "c0";
 	for (uint8_t i = 0; i < model; ++i) {
-		oss << " + ";
-		oss << "c" << i + 1 << " * t";
-		if (i > 0) {
-			oss << "^" << i + 1;
-		}
+		oss << " + t*(c" << i + 1;
+	}
+
+	for (uint8_t i = 0; i < model; ++i) {
+		oss << ")";
 	}
 
 	return optimization_abstract::import_function(oss.str());
--- core/optimization/rational.cpp
+++ core/optimization/rational.cpp
@@ -6,23 +6,22 @@ std::string optimization_rational::import_function(std::vector<uint8_t> model) {
 	if (model.size() == 2) {
 		oss << "(c0";
 		for (uint8_t i = 0; i < model[0]; ++i) {
-			oss << " + ";
-			oss << "c" << i + 1 << " * t";
-			if (i > 0) {
-				oss << "^" << i + 1;
-			}
+			oss << " + t*(c" << i + 1;
+		}
+
+		for (uint8_t i = 0; i < model[0]; ++i) {
+			oss << ")";
 		}
 
 		oss << ") / (1";
 		for (uint8_t i = 0; i < model[1]; ++i) {
-			oss << " + ";
-			oss << "c" << static_cast<uint16_t>(model[0]) + static_cast<uint16_t>(i + 1) << " * t";
-			if (i > 0) {
-				oss << "^" << i + 1;
-			}
+			oss << " + t*(c"
+			    << static_cast<uint16_t>(model[0]) + (i + 1);
 		}
 
-		oss << ")";
+		for (uint8_t i = 0; i < model[1] + 1; ++i) {
+			oss << ")";
+		}
 	}
 
 	return optimization_abstract::import_function(oss.str());
