157e445bd8e136a1e642f1f4b4368ca9c04a0fd0
metrovian <paramedoct@gmail.com>
2026-04-11 01:45:18 +0900

    DELETE. [CMD] - 불필요한 required option 제거

--- command/optimization/rational.cpp
+++ command/optimization/rational.cpp
@@ -5,7 +5,7 @@
 void command_rational::setup(CLI::App *parent) {
 	auto command = parent->add_subcommand("rational")->group("OPTIMIZATION");
 	command->add_option("-f, --func", model_, "rational-approximated function")->required()->expected(2);
-	command->add_option("-i, --in", in_, "measured csv")->required();
+	command->add_option("-i, --in", in_, "measured csv");
 	command->add_option("-n, --iter", iter_, "iteration max")->default_val<size_t>(1000);
 	command->add_option("-e, --eps", eps_, "iteration epsilon")->default_val<double>(1.000E-15);
 	command->add_option("-p, --params", params_, "parameters");
