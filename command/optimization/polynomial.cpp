76f7f1f17f1bfb4ff87ddc844e3d5474387fba5b
metrovian <paramedoct@gmail.com>
2026-04-11 02:27:51 +0900

    CHANGE. [CMD] - 명령어의 설명 변경

--- command/optimization/polynomial.cpp
+++ command/optimization/polynomial.cpp
@@ -4,7 +4,7 @@
 
 void command_polynomial::setup(CLI::App *parent) {
 	auto command = parent->add_subcommand("polynomial")->group("OPTIMIZATION");
-	command->add_option("-f, --func", model_, "polynomial-approximated function")->required();
+	command->add_option("-m, --model", model_, "polynomial-approximated model")->required();
 	command->add_option("-i, --in", in_, "measured csv");
 	command->add_option("-n, --iter", iter_, "iteration max")->default_val<size_t>(1000);
 	command->add_option("-e, --eps", eps_, "iteration epsilon")->default_val<double>(1.000E-15);
