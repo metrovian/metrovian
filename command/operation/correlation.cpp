16816238441db42ca1205e9ef74474eeeeb7446e
metrovian <paramedoct@gmail.com>
2026-04-11 01:35:27 +0900

    CHANGE. [CMD] - 자기상관함수의 리턴을 변경 (자기상관 → 단위파형)

--- command/operation/correlation.cpp
+++ command/operation/correlation.cpp
@@ -4,7 +4,7 @@
 void command_correlation::setup(CLI::App *parent) {
 	auto command = parent->add_subcommand("correlation")->group("OPERATION");
 	command->add_option("-i, --in", in_, "measured wav")->required();
-	command->add_option("-o, --out", out_, "return csv")->required();
+	command->add_option("-o, --out", out_, "return csv");
 	command->callback([this]() { run(); });
 	return;
 }
@@ -14,12 +14,26 @@ void command_correlation::run() {
 	Eigen::VectorXd sample;
 	Eigen::VectorXd acorr;
 	Eigen::VectorXd time;
+	Eigen::VectorXd waveform;
+	Eigen::Index start;
+	Eigen::Index len;
 	int sample_rate = read_audio(in_, sample);
 	if (sample_rate > 0) {
 		acorr = engine.operate(sample);
 		time = Eigen::VectorXd::LinSpaced(acorr.size(), 0.000E+0, (acorr.size() - 1));
 		time /= static_cast<double>(sample_rate);
-		write_vector(out_, time, acorr, ',');
+		for (Eigen::Index i = 1; i < acorr.size() - 1; ++i) {
+			if (acorr[i] > acorr[i - 1] &&
+			    acorr[i] > acorr[i + 1] &&
+			    acorr[i] > 0) {
+				sample.maxCoeff(&start);
+				len = std::min(Eigen::Index(sample_rate * time[i]), sample.size() - start);
+				time = time.head(len);
+				waveform = sample.segment(start, len);
+				write_vector(out_, time, waveform, ',');
+				break;
+			}
+		}
 	}
 
 	return;
